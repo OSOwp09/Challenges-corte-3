79c782021c365fef29dd49aec2a0f22a735438b9
Camilo Osorio <camilo.osorio.ca@gmail.com>
2023-05-26 19:34:32 -0500

    upload challaenge 18

---
+++ challenge18/Server/server.js
@@ -0,0 +1,62 @@
+const express = require("express");
+require("dotenv").config();
+const { dbConnection } = require("../daatabase/config");
+const cors = require("cors");
+
+class Server {
+	constructor() {
+		this.headers = {
+			cors: {
+				origin: "http://127.0.0.1:5500",
+				methods: ["GET", "POST"],
+			},
+		};
+		
+		// Crear express App
+		this.app = express();
+		this.port = process.env.PORT;
+		this.server = require("http").createServer(this.app);
+
+		this.paths = {
+			auth: "/api/auth",
+			task: "/api/task",
+		};
+
+		this.connectToDB();
+		this.addMiddleware();
+		this.setRoutes();
+	}
+
+	//Base de datos
+	async connectToDB() {
+		await dbConnection();
+	}
+
+	addMiddleware() {
+		//CORS
+		this.app.use(cors());
+
+		//Lectura y paseo del body
+		this.app.use(express.json());
+
+		//Public folder
+		this.app.use(express.static("public"));
+	}
+
+	setRoutes() {
+		//Rutas
+		this.app.use(this.paths.auth, require("../routes/auth"));
+		this.app.use(this.paths.task, require("../routes/task"));
+	}
+
+
+	listen() {
+		//Escuchar puerto 4000
+		this.server.listen(this.port, () => {
+			console.log("servidor corriendo en puerto", process.env.PORT);
+		});
+	}
+}
+
+module.exports = Server;
+
